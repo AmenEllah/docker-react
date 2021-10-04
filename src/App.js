b89791a4075579738b519aac6da90910f0027fae
amenellah <amenellah@gmail.com>
2021-10-04 14:05:53 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload v7.
+          Edit <code>src/App.js</code> and save to reload v1.
         </p>
         <a
           className="App-link"
